cf72150be3a0f502c18e7845490786f979ee75b7
Alex Szakal <szakal.alex@gmail.com>
2020-10-13 18:05:13 +0200

    Changed Object2D class

--- TomoLearnS/Object2D.cpp
+++ TomoLearnS/Object2D.cpp
@@ -1,17 +1,10 @@
-#include <CImg.h>
-#include <cstdint>
-#include <string>
-#include <TomoLearnS/Image2D.hpp>
+#include <TomoLearnS/Object2D.hpp>
 
-Image2D::Image2D(){
-	cimg_image = cimg_library::CImg<uint16_t>();
-}
-
-Image2D::Image2D(std::string imageFilePath){
+Object2D::Object2D(std::string imageFilePath, std::array<double, 2> objPixSizes):objPixSizes{objPixSizes}{
 	cimg_image=cimg_library::CImg<uint16_t>(imageFilePath.c_str());
 }
 
-void Image2D::display(std::string title){
+void Object2D::display(std::string title){
 	cimg_window=cimg_library::CImgDisplay(cimg_image, title.c_str());
 	cimg_window.wait();
 }
--- TomoLearnS/Object2D.hpp
+++ TomoLearnS/Object2D.hpp
@@ -1,11 +1,18 @@
 #include <CImg.h>
+#include <string>
+#include <array>
+#include <cstdint>
 
-class Image2D{
+class Object2D{
 public:
-	Image2D(); //Default constructor, initialize an empty image
-	Image2D(std::string imageFilePath);
+	Object2D():objPixSizes{1,1}{  //Default constructor, initialize an empty image
+		cimg_image =cimg_library::CImg<uint16_t>();
+		cimg_window = cimg_library::CImgDisplay();
+	}
+	Object2D(std::string imageFilePath, std::array<double, 2> objPixSizes={0.1, 0.1});
 	void display(std::string title);
 private:
 	cimg_library::CImg<uint16_t> cimg_image;
 	cimg_library::CImgDisplay cimg_window;
+	std::array<double, 2> objPixSizes;
 };
