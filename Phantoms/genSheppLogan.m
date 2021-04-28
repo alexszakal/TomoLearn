e9779a56f91ea53778868e13c0e0fa552ade4010
alexszakal <szakal.alex@gmail.com>
2021-04-28 22:09:02 +0200

    Phantoms changed to represent HU+1000 values

--- Phantoms/genSheppLogan.m
+++ Phantoms/genSheppLogan.m
@@ -1,14 +1,21 @@
 clear all;
+% Script to generate the digital phantoms for reconstruction
+% The phantoms created by the standard Matlab functions then divided by 2.5
+% to get realistic values in [1/cm] units. Then the mu values are converted to
+% Hounsfield units (mu_air ~= 0 was used). 
+% Since the png can handle only positive values and the vacuum has -1000 HU value,
+% 1000 was added to the values. 
 
-[P,E]=phantom('Modified Shepp-Logan', 1024);
+[P,E]=phantom('Shepp-Logan', 1024);
 E(:,1)=[2., -0.98, -0.02, -0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01];
-maxInHU=1000; %Attenuation of bone in HU
-P=uint16(phantom(E, 1024)*maxInHU/2*2);
+mu_water=0.2; %[1/cm]
+%P=uint16(1000*(phantom(E, 1024)/5-mu_water)/mu_water + 1000);
+P=uint16(1000*(P/2.5-mu_water)/mu_water + 1000);
 imshow(P);
-caxis([0 maxInHU])
+caxis([0 max(max(P))])
 title('Shepp-Logan.png')
 figure(2);
-plot(P(822,:));
+plot(int32(P(822,:))-1000);
 title('Shepp-Logan.png')
 imwrite(P, 'SheppLogan.png', 'BitDepth', 16); % 16 bit
 
@@ -18,14 +25,14 @@ P(:,1:130)=[];
 imwrite(P, 'SheppLogan_asymmetric.png', 'BitDepth', 16); %16 bit
 
 
-P2=uint16(phantom('Modified Shepp-Logan', 1024)*maxInHU*2);
+P2=uint16(1000*(phantom('Modified Shepp-Logan', 1024)/2.5-mu_water)/mu_water + 1000);
 figure(3);
 imshow(P2);
 ax=gca;
-caxis(ax, [0 maxInHU])
+caxis([0 max(max(P2))])
 title('ModifiedSheppLogan.png')
 figure(4);
-plot(P2(822,:));
+plot(int32(P2(822,:))-1000);
 title('ModifiedSheppLogan.png')
 imwrite(P2, 'ModifiedSheppLogan.png', 'BitDepth', 16);  %16/15 bit
 
